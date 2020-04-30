85aaca2269e91f79164a42bb3c7752b5fbf836ab
KindsonTheGenius <kindsonthegenius@gmail.com>
2020-04-30 16:44:04 +0200

    Removed imgUrl and added location to Product

--- src/main/java/com/kindsonthegenius/productserver/InitializationComponent.java
+++ src/main/java/com/kindsonthegenius/productserver/InitializationComponent.java
@@ -27,7 +27,7 @@ public class InitializationComponent {
         product.setCode("KAMSUNG-TRIOS");
         product.setTitle("Kamsung Trios 12 inch ");
         product.setDescription("Kamsung Trios 12 Touch");
-        product.setImgUrl("kamsung.jpg");
+        product.setLocation("USA");
         product.setPrice(12000.00);
         productRepository.save(product);
 
@@ -36,8 +36,26 @@ public class InitializationComponent {
         product.setCode("LOKIA-POMIA");
         product.setTitle("Lokia 12 inch" );
         product.setDescription("Lokia Pomia 10");
-        product.setImgUrl("lokia.jpg");
+        product.setLocation("USA");
         product.setPrice(9000.00);
+        productRepository.save(product);
+
+        product=new Product();
+        product.setName("Huawei Spyphone");
+        product.setCode("HUAWEI SP");
+        product.setTitle("Lokia 12 inch" );
+        product.setDescription("Lokia Pomia 10");
+        product.setLocation("USA");
+        product.setPrice(11200.00);
+        productRepository.save(product);
+
+        product=new Product();
+        product.setName("Galaxy s4");
+        product.setCode("Galaxy s4");
+        product.setTitle("Galaxy s4 12 inch" );
+        product.setDescription("Galaxy s4 Pomia 10");
+        product.setLocation("USA");
+        product.setPrice(8800.00);
         productRepository.save(product);
     }
 }
--- src/main/java/com/kindsonthegenius/productserver/models/Product.java
+++ src/main/java/com/kindsonthegenius/productserver/models/Product.java
@@ -12,9 +12,65 @@ public class Product {
     private String code;
     private String title;
     private String description;
-    private String imgUrl;
+    private String location;
     private Double price;
 
     public Product(){
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 }
